b337526902ee679423442655f6404bc27c0762e8
anti-spades <a_valsamos@fanshaweonline.ca>
2022-11-25 23:08:20 -0500

    made scrollable

--- screens/ThirdScreen.js
+++ screens/ThirdScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Alert, Button, Text, View, TextInput, Image} from 'react-native';
+import {Alert, Button, Text, View, TextInput, Image, ScrollView} from 'react-native';
 import {useState} from "react";
 import { styles } from '../styles/styles';
 
 const ThirdScreen = props => {
     return (
+        <ScrollView>
         <><View style={styles.row}>
             <Image style={styles.headerImage} source={require('../images/Silver-Tempest.logo.354.png')} />
             <Text style={styles.textHeader}>Cards in this set: 195  +20 Secret</Text>
@@ -17,6 +18,7 @@ const ThirdScreen = props => {
             <Image style={styles.cards} source={require('../images/Ariados.png')} />
             <Image style={styles.cards} source={require('../images/Sunkern.png')} />
         </View></>
+        </ScrollView>
     );
 }
 
